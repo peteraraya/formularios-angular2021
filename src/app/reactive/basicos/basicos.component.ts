2812b7886e458841c0c6a30e23de566882452976
Pedro <paraya@ticblue.com>
2021-05-05 03:04:20 -0400

    avance de formularios básicos reactivos

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -6,11 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class BasicosComponent implements OnInit {
+export class BasicosComponent {
+  
+  // Estableciendo controles forma normal
+  // miFormulario: FormGroup = new FormGroup({
+  //   nombre     : new FormControl('RTX 4080ti'),
+  //   precio     : new FormControl(1500),
+  //   existencias: new FormControl(5),
+  // });
 
-  constructor() { }
 
-  ngOnInit(): void {
+  // Estableciendo controles con FormBuilder
+  miFormulario:FormGroup = this.fb.group({
+    nombre      :[, [Validators.required,Validators.minLength(3) ]],
+    precio      :[, [Validators.required, Validators.min(0)]],
+    existencias: [, [Validators.required, Validators.min(0)]],
+  });
+  constructor( private fb: FormBuilder) { }
+
+  campoEsValido( campo: string){
+    return this.miFormulario.controls[campo].errors && 
+           this.miFormulario.controls[campo].touched
   }
+
 
 }
